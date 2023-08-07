bdd6771fc460c62df6dd174b25afe86c8fbc3a5a
kevinWu777 <254337598@qq.com>
2023-08-07 21:54:17 +0800

    optimize zlib structure

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #include <sstream>
 #include <iomanip>
 #include "asio.hpp"
+#include "src/zlib.h"
 #include "baseinfo.h"
+
 
 void read_handler(const asio::error_code& error,
                   std::size_t bytes_transferred,
@@ -86,11 +88,48 @@ void start_client() {
   }
 }
 
+void maycompress()
+{
+ const unsigned char pData[] = {
+		123, 34, 37, 83, 24, 2, 98, 178, 57, 220,
+		123, 34, 37, 83, 24, 2, 98, 178, 57, 220,
+		123, 34, 37, 83, 24, 2, 98, 178, 57, 220,
+		123, 34, 37, 83, 24, 2, 98, 178, 57, 220
+	};
+	unsigned long nDataSize = 100;
+
+	//printf("Initial size: %d\n", nDataSize);
+
+	unsigned long nCompressedDataSize = nDataSize;
+	unsigned char * pCompressedData = new unsigned char[nCompressedDataSize];
+	
+	int nResult = compress2(pCompressedData, &nCompressedDataSize, pData, nDataSize, 9);
+
+	if (nResult == Z_OK)
+	{
+		//printf("Compressed size: %d\n", nCompressedDataSize);
+
+		unsigned char * pUncompressedData = new unsigned char[nDataSize];
+		nResult = uncompress(pUncompressedData, &nDataSize, pCompressedData, nCompressedDataSize);
+		if (nResult == Z_OK)
+		{
+			//printf("Uncompressed size: %d\n", nDataSize);
+			if (memcmp(pUncompressedData, pData, nDataSize) == 0)
+				printf("Great Success\n");
+		}
+		delete [] pUncompressedData;
+	}
+
+	delete [] pCompressedData;
+}
 
 
-int main() {
+
+int main()
+{
+  maycompress();
   start_client();
   std::cout << "Reached the end" << std::endl;
- 
   return 0;
 }
+
