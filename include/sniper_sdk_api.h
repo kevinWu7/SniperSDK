2f6cf748e706bab799fda7537d6fdaedc881ed40
kevin_pc <254337598@qq.com>
2023-09-02 00:59:00 +0800

    optimize cmake and export-interface

--- include/sniper_sdk_api.h
+++ include/sniper_sdk_api.h
@@ -1,15 +1,15 @@
 #ifndef SNIPER_SDK_API_H
 #define SNIPER_SDK_API_H
 
+#define SNIPER_SDK_API_EXPORT
 #if defined(__WIN32) || defined(__WIN64)
     #ifdef SNIPER_SDK_API_EXPORT
         #define SNIPER_SDK_API_EXPORT __declspec(dllexport)
     #else
         #define SNIPER_SDK_API_EXPORT __declspec(dllimport)
     #endif
-#else
-    #define SNIPER_SDK_API_EXPORT
 #endif
+
 
 extern "C"
 {
